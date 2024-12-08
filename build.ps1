822789a0593c8ebbf10226e23d2b900b3bcfa0d9
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-08 15:45:27 -0800

    made changes

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ cl /EHsc compiler.cpp
 ren compiler.exe MemoriShaderTool.exe
 mkdir MemoriShaderTool
 move MemoriShaderTool.exe MemoriShaderTool\MemoriShaderTool.exe
-move ShaderConductor.exe MemoriShaderTool\MemoriShaderTool.exe
+move ShaderConductor.exe MemoriShaderTool\ShaderConductor.exe
