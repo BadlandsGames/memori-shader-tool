5574f5c412f654f40014bc9503ba28f6d84841bb
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-08 19:07:29 -0800

    made changes

--- build.ps1
+++ build.ps1
@@ -4,13 +4,14 @@ Write-Host "linux: Linux"
 $choice_os = Read-Host -Prompt "choice"
 Clear-Host
 if($choice_os -eq "win") {
+    Clear-Host
     cl /EHsc compiler.cpp
     ren compiler.exe MemoriShaderTool.exe
     mkdir MemoriShaderTool
     move MemoriShaderTool.exe MemoriShaderTool\MemoriShaderTool.exe
     move ShaderConductor.exe MemoriShaderTool\ShaderConductor.exe
-    Clear-Host
 } elseif($choice_os -eq "linux") {
+    Clear-Host
     g++ -o MemoriShaderTool.elf compiler.cpp
     git clone https://github.com/microsoft/ShaderConductor.git
     cd ShaderConductor
@@ -25,7 +26,6 @@ if($choice_os -eq "win") {
     mv MemoriShaderTool.elf MemoriShaderTool\MemoriShaderTool.elf
     mv ShaderConductor.elf MemoriShaderTool\ShaderConductor.elf
     rm -rf ShaderConductor
-    Clear-Host
 } else {
     Clear-Host
     Write-Host "Error: Operating System doesn't exist."
