5a55ade717e2cbbebf2bf07c2b4969a34cf956bb
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-08 18:40:08 -0800

    made changes

---
+++ compiler.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void compile_file(string inputfile, string outputfile) {
+    string cmd1 = "";
+    string cmd2 = "";
+    cmd1.append("slangc");
+    cmd1.append(" ");
+    cmd1.append(inputfile);
+    cmd1.append(" ");
+    cmd1.append("-target");
+    cmd1.append(" ");
+    cmd1.append("hlsl");
+    cmd1.append(" ");
+    cmd1.append("-o");
+    cmd1.append(" ");
+    cmd1.append(outputfile);
+    cmd2.append("ShaderConductor.exe");
+    cmd2.append(" ");
+    cmd2.append("-i");
+    cmd2.append(" ");
+    cmd2.append(outputfile);
+    cmd2.append(" ");
+    cmd2.append("-o");
+    cmd2.append(" ");
+    cmd2.append("output");
+    cmd2.append(" ");
+    cmd2.append("-t");
+    cmd2.append(" ");
+    cmd2.append("hlsl");
+    cmd2.append(" ");
+    cmd2.append("-s");
+    cmd2.append(" ");
+    cmd2.append("cryengine");
+    system(cmd1);
+    system(cmd2);
+}
